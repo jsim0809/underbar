f3320141d384d9b0d6b8324c143bf887e7e7ab2d
Jeremy Sim <59082457+jsim0809@users.noreply.github.com>
2020-01-06 18:24:57 -0800

    Finished up to uniq. 3 functions left.

--- src/underbar.js
+++ src/underbar.js
@@ -81,16 +81,56 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        if(test(collection[i])) {
+          result.push(collection[i]);
+        }
+      }
+    } else {
+      for (var k in collection) {
+        if(test(collection[k])) {
+          result.push(collection[k]);
+        }
+      };
+    }
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var inverseTest = function(x) {
+      return !test(x);
+    }
+    return _.filter(collection, inverseTest);
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var result = [];
+    if(iterator === undefined) {
+      // Simplest case, unsorted with no iterator
+      var pushIfNotInResult = function(x) {
+        if (_.indexOf(result,x) === -1) {
+          result.push(x);
+        }
+      }
+      _.each(array, pushIfNotInResult);
+    } else {
+      // With iterator
+      var iteratedStuff = [];
+      var pushIfNotInIteratedStuff = function(x) {
+        if (_.indexOf(iteratedStuff,iterator(x)) === -1) {
+          iteratedStuff.push(iterator(x));
+          result.push(x);
+        }
+      }
+      _.each(array, pushIfNotInIteratedStuff);
+    }
+    return result;
   };
 
 
